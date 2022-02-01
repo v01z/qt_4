264736e023c861b23afb71c55c090903697aee15
bcat <v01z@mail.ru>
2022-02-01 22:18:15 +0300

    working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+//    translator.load("./QtLanguage_" + QLocale::system().name());
+    translator.load("./QtLanguage_en");
+    a.installTranslator(&translator);
+
 
     MainWindow w;
     w.show();
